70513cc5e5cf91779dbe2967fd4707fa8b027269
mael-sil <silvestre-siaz.mael@protonmail.com>
2025-04-28 12:03:55 +0200

    premier déploiment

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
-import router from './router'
+import router from '@/router/index'
 
 const app = createApp(App)
 
