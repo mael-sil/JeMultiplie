c9271759e1a44dddad5153b03d9db640d036e40d
mael-sil <silvestre-siaz.mael@protonmail.com>
2025-08-30 18:05:29 +0200

    Enhance ESLint configuration for TypeScript and Vue support
    - Updated ESLint configuration to include TypeScript and Vue parsers.
    - Separated rules and plugins for TypeScript and Vue files for better clarity.
    - Improved language options and parser settings for enhanced linting accuracy.

--- front/eslint.config.ts
+++ front/eslint.config.ts
@@ -1,17 +1,43 @@
-import { vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
+import pluginTypescript from '@typescript-eslint/eslint-plugin'
+import parserTypescript from '@typescript-eslint/parser'
+import parserVue from 'vue-eslint-parser'
 
 export default [
   {
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'node_modules/**'],
   },
   {
-    files: ['**/*.{ts,mts,tsx,vue}'],
-    ...vueTsConfigs.recommended,
-    ...pluginVue.configs['flat/essential'],
+    files: ['**/*.{ts,mts,tsx}'],
     languageOptions: {
+      parser: parserTypescript,
       ecmaVersion: 2022,
       sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': pluginTypescript,
+    },
+    rules: {
+      ...pluginTypescript.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: parserVue,
+      parserOptions: {
+        parser: parserTypescript,
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      vue: pluginVue,
+      '@typescript-eslint': pluginTypescript,
+    },
+    rules: {
+      ...pluginVue.configs['flat/essential'].rules,
+      ...pluginTypescript.configs.recommended.rules,
     },
   },
 ]
