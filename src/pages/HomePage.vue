3f0169a588039eccac1f2bb60dc5bf08be138d01
mael-sil <silvestre-siaz.mael@protonmail.com>
2025-09-22 11:55:49 +0200

    Ajout de la nouvelle section hero sur la page d'accueil

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,32 +1,8 @@
 <script setup lang="ts">
-import { getSave } from '@/composables/save'
-import type { Result } from '@/models/result'
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import StartMenu from '@/components/startMenu.vue'
-import ResultTemplate from '@/components/lastResultTemplate.vue'
+import HeroTemplate from '@/components/heroTemplate.vue';
 
-// Rename component to satisfy multi-word rule
-defineOptions({
-  name: 'HomePage',
-})
-
-const router = useRouter()
-
-const resultHistory = ref<Result[]>(getSave())
-
-function listenStart(): void {
-  router.push('game')
-}
-
-function listenMore(): void {
-  router.push('stats')
-}
-
-console.log(resultHistory.value)
 </script>
 
 <template>
-  <StartMenu @start="listenStart" />
-  <ResultTemplate v-if="resultHistory.length !== 0" :result="resultHistory" @more="listenMore" />
+  <HeroTemplate />
 </template>
